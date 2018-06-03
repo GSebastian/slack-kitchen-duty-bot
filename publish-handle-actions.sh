b6d3be85f8e8c2b453d460a570a1d5b450bdd6cb
Sebastian Ghetu <sebastian@ghetu.net>
2018-06-03 21:14:52 +0100

    Added test/live environments support

--- publish-handle-actions.sh
+++ publish-handle-actions.sh
@@ -1,5 +1,16 @@
-rm index.zip 
+rm index.zip
+
+# Moving the environment files into the lambda function folder
+cp environment.js handle-actions/environment.js
+cp kitchen-duty-423cd-firebase-adminsdk-cvvco-56cbd3f1b5.json handle-actions/kitchen-duty-423cd-firebase-adminsdk-cvvco-56cbd3f1b5.json
+cp kitchen-duty-test-firebase-adminsdk-dxpbl-cf1bc3eb3b.json handle-actions/kitchen-duty-test-firebase-adminsdk-dxpbl-cf1bc3eb3b.json
+
 cd handle-actions 
 zip "../index.zip" * -r -X
 cd .. 
 aws lambda update-function-code --function-name slack-kitchen-duty-bot-handle-actions --zip-file fileb://index.zip
+
+# Cleaning up the environment files
+rm handle-actions/environment.js
+rm handle-actions/kitchen-duty-423cd-firebase-adminsdk-cvvco-56cbd3f1b5.json
+rm handle-actions/kitchen-duty-test-firebase-adminsdk-dxpbl-cf1bc3eb3b.json
